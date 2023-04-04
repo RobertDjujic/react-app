3209a1a54e79be97f76fb820684ccdfeafa5b10f
Robert <robert.djujic@gmail.com>
2023-04-04 18:56:45 +0200

    Added new Flex exercise

--- src/components/flex-exe.tsx
+++ src/components/flex-exe.tsx
@@ -29,10 +29,57 @@ const FlexExe = () => {
           <h3>Lorem Ipsum</h3>
         </div>
       </div>
-      <div className="flex__wrapper">
-        <div></div>
-        <div></div>
-        <div></div>
+      <div className="flex__wrapper special">
+        <div className="flex flex--fourth">
+          <div>
+            <h3>Lorem Ipsum</h3>
+          </div>
+          <div className="flex--fourth__text">
+            {" "}
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </div>
+          <div>
+            <button className="button">Lorem Ipsum</button>
+          </div>
+        </div>
+        <div className="flex flex--fourth">
+          <div>
+            <h3>Lorem Ipsum</h3>
+          </div>
+          <div className="flex--fourth__text">
+            {" "}
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </div>
+          <div>
+            <button className="button">Lorem Ipsum</button>
+          </div>
+        </div>
+        <div className="flex flex--fourth">
+          <div>
+            <h3>Lorem Ipsum</h3>
+          </div>
+          <div className="flex--fourth__text">
+            {" "}
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+          </div>
+          <div>
+            <button className="button">Lorem Ipsum</button>
+          </div>
+        </div>
       </div>
     </>
   );
