1bd621eb4c84350284bd403e0cc028d60fc32490
Robert Đujić <robert.djujic@gmail.com>
2023-04-27 00:00:37 +0200

    Add loop tasks solutions

--- src/pages/petlje.tsx
+++ src/pages/petlje.tsx
@@ -27,6 +27,51 @@ const Petlje = () => {
       x.push(i);
     }
     console.log(x);*/
+    //5. Ispiši brojeve od 0 do zadane vrijednosti
+    /*for (let i = 0; i <= inputValue; i++) {
+      console.log(i);
+    }*/
+    //6. Ispiši brojeve od 12 do zadane vrijednosti
+    /*for (let i = 12; i <= inputValue; i++) {
+      console.log("Vrijednost i je: " + i);
+    }*/
+    //7. Ispiši neparne brojeve od 7 do zadanog broja
+    /*for (let i = 7; i <= inputValue; i++) {
+      if (i % 2) {
+        console.log("Vrijednost i je: " + i);
+      }
+    }*/
+    //8. Ispiši parne brojeve od 10 do zadane negativne vrijednosti
+    /*for (let i = 10; i >= inputValue; i--) {
+      if (!(i % 2)) {
+        console.log("Vrijednost i je: " + i);
+      }
+    }*/
+    /*9. Iteriraj kroz sve brojeve od 1 do zadane vrijednosti. Ispiši sljedeće:
+    - za višekratnike broja 3 ispiši "Fizz"
+    - za višekratnike broja 5 ispiši "Buzz"
+    - za višekratnike broja 3 i 5 ispiši "FizzBuzz"*/
+    /*for (let i = 1; i <= inputValue; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        console.log(i + " FizzBuzz");
+      } else if (i % 3 === 0) {
+        console.log(i + " Fizz");
+      } else if (i % 5 === 0) {
+        console.log(i + " Buzz");
+      }
+    }*/
+    //10. Ispiši sve elemente sljedećeg array-a
+    /*const thisIsAnArray = ["element1", "element2", "element3", "element4"];
+    thisIsAnArray.forEach((element) => {
+      console.log(element);
+    });*/
+    //11. Izračunaj zbroj svih brojeva u array-u
+    /*const numbersArray = [1, 13, 22, 123, 49];
+    let sum = 0;
+    for (let i = 0; i < numbersArray.length; i++) {
+      sum += numbersArray[i];
+      console.log("Zbroj brojeva u array-u iznosi: " + sum);
+    }*/
   };
   return (
     <div className="container">
