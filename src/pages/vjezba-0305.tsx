f6a8f5081d2e8395ccf65b10314914630d877f38
Robert Đujić <robert.djujic@gmail.com>
2023-05-07 17:18:01 +0200

    Remove sum function from vjezba-0305

--- src/pages/vjezba-0305.tsx
+++ src/pages/vjezba-0305.tsx
@@ -1,18 +1,14 @@
 const Vjezba0305 = () => {
   const handleClick = () => {
     let box = document.getElementsByClassName("box")[0] as HTMLDivElement;
-    box.style.transform = "scale(0.5)";
-  };
-
-  const sum = (x: number, y: number) => {
-    return x + y;
+    box.style.transform = "scale(0.1)";
   };
 
   return (
     <div className="container">
       <h1>Vježba 0305</h1>
       <hr />
-      <div className="box">{sum(5, 10)}</div>
+      <div className="box"></div>
       <button onClick={() => handleClick()}>Click me</button>
     </div>
   );
