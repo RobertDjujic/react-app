0458620d8d430f197306eedf216a22d3e49bb8e5
Robert Đujić <robert.djujic@gmail.com>
2023-05-26 17:44:16 +0200

    Add ascending/descending button, modal function and overlay to game-genres

---
+++ src/data/genres.ts
@@ -0,0 +1,154 @@
+import { GenreType } from "../pages/game-genres";
+
+export const genres: GenreType[] = [
+  {
+    img: "https://store.steampowered.com/categories/homepageimage/category/exploration_open_world?cc=us&l=english",
+    name: "Open World",
+    id: 1,
+    games: [
+      {
+        img: "https://cdn.akamai.steamstatic.com/steam/apps/1774580/header.jpg?t=1684344870",
+        name: "Star Wars Jedi: Survivor",
+        desc: "The story of Cal Kestis continues in STAR WARS Jedi: Survivor™, a galaxy-spanning, third-person, action-adventure game.",
+        price: 69.99,
+      },
+      {
+        img: "https://cdn.akamai.steamstatic.com/steam/apps/990080/header.jpg?t=1680647016",
+        name: "Hogwarts Legacy",
+        desc: "Hogwarts Legacy is an immersive, open-world action RPG. Now you can take control of the action and be at the center of your own adventure in the wizarding world.",
+        price: 59.99,
+      },
+      {
+        img: "https://cdn.akamai.steamstatic.com/steam/apps/1245620/header.jpg?t=1683618443",
+        name: "Elden Ring",
+        desc: "THE NEW FANTASY ACTION RPG. Rise, Tarnished, and be guided by grace to brandish the power of the Elden Ring and become an Elden Lord in the Lands Between.",
+        price: 59.99,
+      },
+    ],
+  },
+  {
+    img: "https://store.steampowered.com/categories/homepageimage/category/science_fiction?cc=us&l=english",
+    name: "Sci-Fi & Cyberpunk",
+    id: 2,
+    games: [
+      {
+        img: "https://cdn.akamai.steamstatic.com/steam/apps/1774580/header.jpg?t=1684344870",
+        name: "Star Wars Jedi: Survivor",
+        desc: "The story of Cal Kestis continues in STAR WARS Jedi: Survivor™, a galaxy-spanning, third-person, action-adventure game.",
+        price: 69.99,
+      },
+      {
+        img: "https://cdn.akamai.steamstatic.com/steam/apps/1091500/header.jpg?t=1680026109",
+        name: "Cyberpunk 2077",
+        desc: "Cyberpunk 2077 is an open-world, action-adventure RPG set in the dark future of Night City — a dangerous megalopolis obsessed with power, glamor, and ceaseless body modification.",
+        price: 59.99,
+      },
+      {
+        img: "https://cdn.akamai.steamstatic.com/steam/apps/782330/header.jpg?t=1661971606",
+        name: "DOOM Eternal",
+        desc: "Hell’s armies have invaded Earth. Become the Slayer in an epic single-player campaign to conquer demons across dimensions and stop the final destruction of humanity. The only thing they fear... is you.",
+        price: 39.99,
+      },
+    ],
+  },
+  {
+    img: "https://store.steampowered.com/categories/homepageimage/category/simulation?cc=us&l=english",
+    name: "Simulation",
+    id: 3,
+    games: [
+      {
+        img: "https://cdn.akamai.steamstatic.com/steam/apps/255710/header.jpg?t=1683626322",
+        name: "Cities: Skylines",
+        desc: "Cities: Skylines is a modern take on the classic city simulation. The game introduces new game play elements to realize the thrill and hardships of creating and maintaining a real city whilst expanding on some well-established tropes of the city building experience.",
+        price: 27.99,
+      },
+      {
+        img: "https://cdn.akamai.steamstatic.com/steam/apps/1158310/header.jpg?t=1683820745",
+        name: "Crusader Kings III",
+        desc: "Love, fight, scheme, and claim greatness. Determine your noble house’s legacy in the sprawling grand strategy of Crusader Kings III. Death is only the beginning as you guide your dynasty’s bloodline in the rich and larger-than-life simulation of the Middle Ages.",
+        price: 49.99,
+      },
+      {
+        img: "https://cdn.akamai.steamstatic.com/steam/apps/289070/header.jpg?t=1680898825",
+        name: "Sid Meier's Civilization VI",
+        desc: "Civilization VI is the newest installment in the award winning Civilization Franchise. Expand your empire, advance your culture and go head-to-head against history’s greatest leaders. Will your civilization stand the test of time?",
+        price: 59.99,
+      },
+    ],
+  },
+  {
+    img: "https://store.steampowered.com/categories/homepageimage/category/rpg?cc=us&l=english",
+    name: "Role-Playing",
+    id: 4,
+    games: [
+      {
+        img: "https://cdn.akamai.steamstatic.com/steam/apps/435150/header.jpg?t=1668591196",
+        name: "Divinity: Original Sin 2",
+        desc: "The critically acclaimed RPG that raised the bar, from the creators of Baldur's Gate 3. Gather your party. Master deep, tactical combat. Venture as a party of up to four - but know that only one of you will have the chance to become a God.",
+        price: 44.99,
+      },
+      {
+        img: "https://cdn.akamai.steamstatic.com/steam/apps/1328670/header.jpg?t=1669773470",
+        name: "Mass Effect Legendary Edition",
+        desc: "The Mass Effect™ Legendary Edition includes single-player base content and over 40 DLC from the highly acclaimed Mass Effect, Mass Effect 2, and Mass Effect 3 games, including promo weapons, armors, and packs — remastered and optimized for 4K Ultra HD.",
+        price: 59.99,
+      },
+      {
+        img: "https://cdn.akamai.steamstatic.com/steam/apps/292030/header.jpg?t=1675178392",
+        name: "The Witcher 3: Wild Hunt",
+        desc: "You are Geralt of Rivia, mercenary monster slayer. Before you stands a war-torn, monster-infested continent you can explore at will. Your current contract? Tracking down Ciri — the Child of Prophecy, a living weapon that can alter the shape of the world.",
+        price: 29.99,
+      },
+    ],
+  },
+  {
+    img: "https://store.steampowered.com/categories/homepageimage/category/strategy?cc=us&l=english",
+    name: "Strategy",
+    id: 5,
+    games: [
+      {
+        img: "https://cdn.akamai.steamstatic.com/steam/apps/1669000/header.jpg?t=1683635102",
+        name: "Age of Wonders 4",
+        desc: "Rule a fantasy realm of your own design! Explore new magical realms in Age of Wonders’ signature blend of 4X strategy and turn-based tactical combat. Control a faction that grows and changes as you expand your empire with each turn!",
+        price: 49.99,
+      },
+      {
+        img: "https://cdn.akamai.steamstatic.com/steam/apps/570/header.jpg?t=1682639497",
+        name: "Dota 2",
+        desc: "Every day, millions of players worldwide enter battle as one of over a hundred Dota heroes. And no matter if it's their 10th hour of play or 1,000th, there's always something new to discover. With regular updates that ensure a constant evolution of gameplay, features, and heroes, Dota 2 has taken on a life of its own.",
+        price: 0,
+      },
+      {
+        img: "https://cdn.akamai.steamstatic.com/steam/apps/1142710/header.jpg?t=1681400832",
+        name: "Total War: WARHAMMER III",
+        desc: "The cataclysmic conclusion to the Total War: WARHAMMER trilogy is here. Rally your forces and step into the Realm of Chaos, a dimension of mind-bending horror where the very fate of the world will be decided. Will you conquer your Daemons… or command them?",
+        price: 59.99,
+      },
+    ],
+  },
+  {
+    img: "https://store.steampowered.com/categories/homepageimage/category/horror?cc=us&l=english",
+    name: "Horror",
+    id: 6,
+    games: [
+      {
+        img: "https://cdn.akamai.steamstatic.com/steam/apps/2050650/header.jpg?t=1684904145",
+        name: "Resident Evil 4",
+        desc: "Survival is just the beginning. Six years have passed since the biological disaster in Raccoon City. Leon S. Kennedy, one of the survivors, tracks the president's kidnapped daughter to a secluded European village, where there is something terribly wrong with the locals.",
+        price: 59.99,
+      },
+      {
+        img: "https://cdn.akamai.steamstatic.com/steam/apps/1693980/header.jpg?t=1684172906",
+        name: "Dead Space",
+        desc: "The sci-fi survival-horror classic returns, completely rebuilt to offer an even more immersive experience — including visual, audio, and gameplay improvements — while staying faithful to the original game’s thrilling vision.",
+        price: 59.99,
+      },
+      {
+        img: "https://cdn.akamai.steamstatic.com/steam/apps/1196590/header.jpg?t=1681094558",
+        name: "Resident Evil Village",
+        desc: "Experience survival horror like never before in the 8th major installment in the Resident Evil franchise - Resident Evil Village. With detailed graphics, intense first-person action and masterful storytelling, the terror has never felt more realistic.",
+        price: 39.99,
+      },
+    ],
+  },
+];
--- src/pages/game-genres.tsx
+++ src/pages/game-genres.tsx
@@ -1,84 +1,127 @@
 import { useState } from "react";
-import { gameGenres } from "../data/gameGenres";
+import { genres } from "../data/genres";
+import ArrowDown from "../assets/arrow-down-a-z-solid";
+import ArrowUp from "../assets/arrow-up-a-z-solid";
+import XMark from "../assets/x-mark";
 
-export type GameGenresType = {
-  genreImg: string;
-  genreName: string;
-  genreGames: GameInfoType[];
+export type GenreType = {
+  games: GameType[];
   id: number;
+  img: string;
+  name: string;
 };
 
-type GameInfoType = {
-  gameImg: string;
-  gameName: string;
-  gameDesc: string;
-  gamePrice: number;
+type GameType = {
+  desc: string;
+  img: string;
+  name: string;
+  price: number;
 };
 
 const GameGenres = () => {
   const [modal, setModal] = useState<boolean>(false);
-  const [activeGenre, setActiveGenre] = useState<GameInfoType[] | null>(null);
+  const [activeGenre, setActiveGenre] = useState<GameType[] | null>(null);
+  const [activeOrder, setActiveOrder] = useState<boolean>(false);
+  const [ascendingOrder, setAscendingOrder] = useState<boolean>();
 
   const findGames = (id: number) => {
-    gameGenres.forEach((genre) => {
+    genres.forEach((genre) => {
       if (genre.id === id) {
-        setActiveGenre(genre.genreGames);
+        setActiveGenre(genre.games);
       }
     });
   };
 
+  const handleOrder = () => {
+    const newGameList = [...genres];
+    newGameList.forEach((genre) => {
+      genre.games.sort((a, b) => {
+        return ascendingOrder
+          ? b.name.localeCompare(a.name)
+          : a.name.localeCompare(b.name);
+      });
+    });
+
+    setAscendingOrder(!ascendingOrder);
+  };
+
   return (
-    <div className="container">
-      <h1>Game Genres</h1>
-      <div className="game-genres">
-        <div className="game-genres__item__grid">
-          {gameGenres.map((genre: GameGenresType) => {
-            return (
-              <div
-                onClick={() => {
-                  findGames(genre.id);
-                }}
-                className="game-genres__item"
-              >
-                <img
-                  className="game-genres__item__img"
-                  src={genre.genreImg}
-                  alt="An image representing a specific game genre."
-                />
-                <div className="game-genres__item__gradient"></div>
-                <div className="game-genres__item__label">
-                  {genre.genreName}
+    <>
+      <div className={modal ? "overlay" : ""}></div>
+      <div className="container">
+        <h1>Game Genres</h1>
+        <div className="game-genres">
+          <div className="game-genres__item__grid">
+            {genres.map((genre: GenreType) => {
+              return (
+                <div
+                  className="game-genres__item"
+                  onClick={() => {
+                    findGames(genre.id);
+                    setModal(!modal);
+                  }}
+                >
+                  <img
+                    className="game-genres__item__img"
+                    src={genre.img}
+                    alt="An image representing a specific game genre."
+                  />
+                  <div className="game-genres__item__gradient"></div>
+                  <div className="game-genres__item__label">{genre.name}</div>
                 </div>
-              </div>
-            );
-          })}
-        </div>
-        <div className="game-genres__modal">
-          {activeGenre
-            ? activeGenre.map((genre: GameInfoType) => {
-                return (
-                  <div className="game-genres__modal__item">
-                    <div className="game-genres__modal__item__img">
-                      <img src={genre.gameImg} alt="An image of a game." />
-                    </div>
-                    <div>
-                      <div className="game-genres__modal__item__name">
-                        {genre.gameName}
+              );
+            })}
+          </div>
+          <div
+            className={
+              modal ? "game-genres__modal" : "game-genres__modal hidden"
+            }
+          >
+            <div
+              className="game-genres__modal__close"
+              onClick={() => {
+                setModal(false);
+              }}
+            >
+              <XMark />
+            </div>
+            <div
+              className="game-genres__modal__order"
+              onClick={() => {
+                setActiveOrder(!activeOrder);
+                handleOrder();
+              }}
+            >
+              {activeOrder ? <ArrowDown /> : <ArrowUp />}
+            </div>
+            <div>
+              {activeGenre
+                ? activeGenre.map((game: GameType) => {
+                    return (
+                      <div className="game-genres__modal__item">
+                        <div className="game-genres__modal__item__img">
+                          <img src={game.img} alt="An image of a game." />
+                        </div>
+                        <div>
+                          <div className="game-genres__modal__item__name">
+                            {game.name}
+                          </div>
+                          <div className="game-genres__modal__item__desc">
+                            {game.desc}
+                          </div>
+                          <span className="game-genres__modal__item__price">
+                            {`${game.price}€`}
+                          </span>
+                        </div>
                       </div>
-                      <div className="game-genres__modal__item__desc">
-                        {genre.gameDesc}
-                      </div>
-                      <span className="game-genres__modal__item__price">
-                        {`${genre.gamePrice}€`}
-                      </span>
-                    </div>
-                  </div>
-                );
-              })
-            : "Nije ništa odabrano."}
+                    );
+                  })
+                : "Nothing has been selected."}
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
